d8e63c91ac3bf1c8c96d2d6ec453d2fe667451e0
Kuldeep Singh <kuldeeps@INkuldeeps.local>
2018-08-27 17:27:24 +0530

    [Manisha/Kuldeep]Record Extras against Current Bowler

---
+++ src/store/ExtrasReducer.test.js
@@ -0,0 +1,30 @@
+import {ACTION_BYE, ACTION_LEG_BYE, ACTION_NO_BALL, ACTION_WIDE, ExtrasReducer} from "./ExtrasReducer"
+
+describe('ExtrasReducer', () => {
+    it('should return initial state of all extras unselected', () => {
+        const initialState = {
+            wideSelected : false,
+            noBallSelected : false,
+            byeSelected : false,
+            legByeSelected : false
+        };
+        expect(ExtrasReducer(undefined, {})).toEqual(initialState);
+    });
+
+    it('should toggle wide when wide in extras is selected', () => {
+        expect(ExtrasReducer(undefined, ACTION_WIDE ).wideSelected).toEqual(true);
+    });
+
+    it('should toggle noBall when noBall in extras is selected', () => {
+        expect(ExtrasReducer(undefined, ACTION_NO_BALL ).noBallSelected).toEqual(true);
+    });
+
+    it('should toggle bye when bye in extras is selected', () => {
+        expect(ExtrasReducer(undefined, ACTION_BYE ).byeSelected).toEqual(true);
+    });
+
+    it('should toggle legBye when noBall in legBye is selected', () => {
+        expect(ExtrasReducer(undefined, ACTION_LEG_BYE ).legByeSelected).toEqual(true);
+    });
+});
+
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import gameInformationReducer from '../newGame/reducer';
+import {ExtrasReducer} from "./ExtrasReducer";
 
 const rootReducer = combineReducers({
   gameInformation: gameInformationReducer,
+  extras:ExtrasReducer
 });
 
 export default rootReducer;
