07a1ec0aea714654e4684a9ff7061b9574db885a
Harshagr <harshagr@Harshagrs-MacBook-Pro.local>
2018-08-28 15:40:07 +0530

    Fixing lint error

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -8,13 +8,14 @@ import './Home.css';
 import { createGameAction } from './actions';
 
 const Home = props =>
-  (<Container className="h-100">
-    <Row className="align-items-center h-100">
-      <Col className="text-center">
-        <Button color="primary" onClick={props.createGame}>Create Game</Button>
-      </Col>
-    </Row>
-  </Container>);
+  (
+    <Container className="h-100">
+      <Row className="align-items-center h-100">
+        <Col className="text-center">
+          <Button color="primary" onClick={props.createGame}>Create Game</Button>
+        </Col>
+      </Row>
+    </Container>);
 
 const mapDispatchToProps = dispatch => ({
   createGame: () => dispatch(createGameAction()),
--- src/scorer/Extras.jsx
+++ src/scorer/Extras.jsx
@@ -1,31 +1,58 @@
+import { connect } from 'react-redux';
 import React from 'react';
-import {ACTION_BYE, ACTION_LEG_BYE, ACTION_NO_BALL, ACTION_WIDE} from "../store/ExtrasReducer";
-import {connect} from "react-redux"
+import { ACTION_BYE, ACTION_LEG_BYE, ACTION_NO_BALL, ACTION_WIDE } from '../store/ExtrasReducer';
 import './Extras.css';
 
-const Extras = (props) => (
+const Extras = props => (
   <div>
-      <button className={props.wideSelected?"selected":""}  name = {ACTION_WIDE.type}
-              onClick={(event) => {props.toggle(event.target.name)}}>W</button>
-      <button className={props.noBallSelected?"selected":""} name = {ACTION_NO_BALL.type} onClick={(event) => {props.toggle(event.target.name)}}>NB</button>
-      <button className={props.byeSelected?"selected":""} name = {ACTION_BYE.type} onClick={(event) => {props.toggle(event.target.name)}}>B</button>
-      <button className={props.legByeSelected?"selected":""} name = {ACTION_LEG_BYE.type} onClick={(event) => {props.toggle(event.target.name)}}>LB</button>
-      <button onClick={(event) => {props.toggle(event.target.name)}}>WB</button>
+    <button
+      className={props.wideSelected ? 'selected' : ''}
+      name={ACTION_WIDE.type}
+      onClick={(event) => { props.toggle(event.target.name); }}
+    >
+      W
+    </button>
+    <button
+      className={props.noBallSelected ? 'selected' : ''}
+      name={ACTION_NO_BALL.type}
+      onClick={(event) => { props.toggle(event.target.name); }}
+    >
+      NB
+    </button>
+    <button
+      className={props.byeSelected ? 'selected' : ''}
+      name={ACTION_BYE.type}
+      onClick={(event) => { props.toggle(event.target.name); }}
+    >
+      B
+    </button>
+    <button
+      className={props.legByeSelected ? 'selected' : ''}
+      name={ACTION_LEG_BYE.type}
+      onClick={(event) => { props.toggle(event.target.name); }}
+    >
+      LB
+    </button>
+    <button
+      onClick={(event) => { props.toggle(event.target.name); }}
+    >
+      WB
+    </button>
   </div>
 );
 
-const mapStateToProps=(state)=> ({
-    wideSelected : state.extras.wideSelected,
-    noBallSelected : state.extras.noBallSelected,
-    byeSelected : state.extras.byeSelected,
-    legByeSelected : state.extras.legByeSelected
+const mapStateToProps = state => ({
+  wideSelected: state.extras.wideSelected,
+  noBallSelected: state.extras.noBallSelected,
+  byeSelected: state.extras.byeSelected,
+  legByeSelected: state.extras.legByeSelected,
 });
 
 
-const mapPropsToDispatcher=(dispatch)=>({
-    toggle : (name)=>dispatch({type:name})
+const mapPropsToDispatcher = dispatch => ({
+  toggle: name => dispatch({ type: name }),
 });
 
+const ExtrasConnectComponent = connect(mapStateToProps, mapPropsToDispatcher)(Extras);
 
-const ExtrasConnectComponent=connect(mapStateToProps,mapPropsToDispatcher)(Extras);;
-export { ExtrasConnectComponent }
+export default ExtrasConnectComponent;
