1a9e447bb04693c709affcc389f68f396456550a
Prakash Vadrevu <prakashv@thoughtworks.com>
2018-08-28 12:35:50 +0530

    Over: function to check for the over completion

--- src/model/over.js
+++ src/model/over.js
@@ -1,12 +1,23 @@
+
+import { BALL_TYPE_REGULAR } from '../store/BallPlayedReducer';
+
+export const BALLS_PER_OVER = 6;
+
 class Over {
   constructor(bowler) {
     this.bowler = bowler;
     this.balls = [];
   }
 
-  addBall() {
-    this.balls = [];
-    // do nothing
+  addBall(ball) {
+    this.balls.push(ball);
+  }
+
+  isComplete() {
+    const regularBalls = this.balls.reduce((accumulator, ball) => {
+      return accumulator + (BALL_TYPE_REGULAR === ball.type ? 1 : 0);
+    }, 0);
+    return BALLS_PER_OVER === regularBalls;
   }
 }
 
---
+++ src/model/over.test.js
@@ -0,0 +1,49 @@
+import Over from './over';
+import { BALL_TYPE_REGULAR, BALL_TYPE_WIDE, BALL_TYPE_NO_BALL } from '../store/BallPlayedReducer';
+
+describe('over', () => {
+  function ball(type, playerRuns, teamRuns, extraBall, out) {
+    return {
+      type, playerRuns, teamRuns, extraBall, out,
+    };
+  }
+
+  it('over should not complete until the REGULAR ball count reaches 6', () => {
+    const over = new Over();
+
+    over.addBall(ball(BALL_TYPE_WIDE, 0, 1, 1, false));
+    over.addBall(ball(BALL_TYPE_REGULAR, 3, 3, 0, false));
+    over.addBall(ball(BALL_TYPE_NO_BALL, 6, 7, 1, false));
+    over.addBall(ball(BALL_TYPE_REGULAR, 1, 1, 0, false));
+    over.addBall(ball(BALL_TYPE_REGULAR, 0, 0, 0, true));
+
+    expect(over.isComplete()).toBe(false);
+  });
+
+  it('over should complete if all the first 6 balls are REGULAR balls', () => {
+    const over = new Over();
+
+    over.addBall(ball(BALL_TYPE_REGULAR, 0, 1, 0, false));
+    over.addBall(ball(BALL_TYPE_REGULAR, 3, 3, 0, false));
+    over.addBall(ball(BALL_TYPE_REGULAR, 6, 7, 0, false));
+    over.addBall(ball(BALL_TYPE_REGULAR, 1, 1, 0, false));
+    over.addBall(ball(BALL_TYPE_REGULAR, 0, 0, 0, true));
+    over.addBall(ball(BALL_TYPE_REGULAR, 0, 0, 0, true));
+
+    expect(over.isComplete()).toBe(true);
+  });
+
+  it('over should complete if all the first 6 balls are REGULAR balls', () => {
+    const over = new Over();
+
+    over.addBall(ball(BALL_TYPE_REGULAR, 0, 1, 0, false));
+    over.addBall(ball(BALL_TYPE_REGULAR, 3, 3, 0, false));
+    over.addBall(ball(BALL_TYPE_WIDE, 0, 0, 1, false));
+    over.addBall(ball(BALL_TYPE_REGULAR, 1, 1, 0, false));
+    over.addBall(ball(BALL_TYPE_REGULAR, 0, 0, 0, true));
+    over.addBall(ball(BALL_TYPE_REGULAR, 0, 0, 0, true));
+
+    expect(over.isComplete()).toBe(false);
+  });
+});
+
