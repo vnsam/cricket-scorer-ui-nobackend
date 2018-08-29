bb9e462c22a61a200d48cbe06b9c921b06d1e35f
Harshagr <harshagr@Harshagrs-MacBook-Pro.local>
2018-08-29 15:49:27 +0530

    Story 1 - with updated test cases

--- src/newGame/NewGame.js
+++ src/newGame/NewGame.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NewGame = () => (
   <div>
-    <p>dummy</p>
+    dummy
   </div>
 );
 
---
+++ src/scorer/CurrentScoreReducer.test.js
@@ -0,0 +1,64 @@
+import scoreReducer from './CurrentScoreReducer';
+import { BALL_TYPE_REGULAR, ACTION_BALL_PLAYED } from '../store/BallResultReducer';
+
+describe('currentScore', () => {
+  function mockBallPlayedData(run, wicket) {
+    return {
+      battingTeam: {
+        name: 'Team 1',
+        runs: run,
+        wickets: wicket,
+        overBowled: 0,
+        totalOvers: 20,
+      },
+      bowlingTeam: {
+        name: 'Team 2',
+        runs: 180,
+        wickets: 6,
+        overBowled: 20,
+        totalOvers: 20,
+      },
+    };
+  }
+
+  function mockBallPlayedAction(teamRun, playerRun, isOut) {
+    return {
+      type: ACTION_BALL_PLAYED().type,
+      data: {
+        type: BALL_TYPE_REGULAR,
+        extraBall: 0,
+        teamRuns: teamRun,
+        playerRuns: playerRun,
+        out: isOut,
+      },
+    };
+  }
+
+  it('should return initial state', () => {
+    expect(scoreReducer(
+      mockBallPlayedData(0, 0),
+      mockBallPlayedAction(0, 0, false),
+    )).toEqual(mockBallPlayedData(0, 0));
+  });
+
+  it('should return total runs as 1 when ball played with teamRun 1 and playerRun 0', () => {
+    expect(scoreReducer(
+      mockBallPlayedData(0, 0),
+      mockBallPlayedAction(1, 0, false),
+    )).toEqual(mockBallPlayedData(1, 0));
+  });
+
+  it('should return total runs as 1 when ball played with teamRun 1 and playerRun 1', () => {
+    expect(scoreReducer(
+      mockBallPlayedData(0, 0),
+      mockBallPlayedAction(1, 1, false),
+    )).toEqual(mockBallPlayedData(2, 0));
+  });
+
+  it('should return total runs as 0 and wicket is 1 when ball played is bowled', () => {
+    expect(scoreReducer(
+      mockBallPlayedData(0, 0), // initial state
+      mockBallPlayedAction(0, 0, true), // played action
+    )).toEqual(mockBallPlayedData(0, 1)); // final state
+  });
+});
