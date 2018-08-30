74aaaddaeea10e02e0275e0093d4aa9af2999214
manishaj <manishaj@thoughtworks.com>
2018-08-30 12:33:46 +0530

    Updated import for OnStrikeReducer in rootReducer.js

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -4,7 +4,7 @@ import { BallResultReducer } from './BallResultReducer';
 import batsmanReducer from './BatsmenStatsReducer';
 import OverReducer from '../scorer/OverReducer';
 import scoreReducer from '../scorer/CurrentScoreReducer';
-import { OnStrikeReducer } from '../store/OnStrikeChangeReducer';
+import OnStrikeReducer from '../store/OnStrikeChangeReducer';
 
 const rootReducer = combineReducers({
   gameInformation: gameInformationReducer,
